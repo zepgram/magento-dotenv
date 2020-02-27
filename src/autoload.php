a1af2f7f45eeae645cb0bed1cebb95b3dcbe4c09
Benjamin Calef <benjamin.calef@caudalie.com>
2020-02-27 22:03:14 +0100

    Magento dotenv implementation

---
+++ src/autoload.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once BP . '/app/dotenv.php';
--- src/dist/app/dotenv.php
+++ src/dist/app/dotenv.php
@@ -2,5 +2,4 @@
 use Symfony\Component\Dotenv\Dotenv;
 
 $dotenv = new Dotenv(true);
-$baseDir = dirname(__DIR__) . 'dotenv.php/';
-$dotenv->loadEnv($baseDir . '.env');
+$dotenv->loadEnv(BP . '/.env');
