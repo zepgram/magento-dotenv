3ed010fe8b632be9874a315b55d78373b5bd2640
Benjamin Calef <zepgram@gmail.com>
2020-09-02 09:26:42 +0200

    [autoload] add test to create dotenv file only on magento installation

--- MagentoDotenv.php
+++ MagentoDotenv.php
@@ -6,26 +6,39 @@ use Symfony\Component\Dotenv\Dotenv as SymfonyDotenv;
 class MagentoDotenv
 {
     /** @var string */
-    public const SRC_APP_ETC_DOTENV = '/vendor/zepgram/magento-dotenv/dist/app/dotenv.php';
+    public const MAGENTO_BOOTSTRAP_FILE = 'app/bootstrap.php';
 
     /** @var string */
-    public const DEST_APP_ETC_DOTENV = '/app/dotenv.php';
+    public const SRC_APP_ETC_DOTENV_FILE = 'vendor/zepgram/magento-dotenv/dist/app/dotenv.php';
 
     /** @var string */
-    public const ENV_FILE = '/.env';
+    public const DEST_APP_ETC_DOTENV_FILE = 'app/dotenv.php';
+
+    /** @var string */
+    public const ENV_FILE = '.env';
+
+    /** @var string */
+    public const ENV_KEY = 'APP_ENV';
+
+    /** @var string */
+    public const DEBUG_KEY = 'APP_DEBUG';
 
     /**
      * Deploy dotenv files and load dotenv
      */
     public static function init()
     {
-        $dotenvSrc = ROOT_DIRECTORY . self::SRC_APP_ETC_DOTENV;
-        $dotenvDest = ROOT_DIRECTORY . self::DEST_APP_ETC_DOTENV;
-        $envFile = ROOT_DIRECTORY . self::ENV_FILE;
+        if (!file_exists(ROOT_DIRECTORY . self::MAGENTO_BOOTSTRAP_FILE)) {
+            return;
+        }
 
+        $dotenvSrc = ROOT_DIRECTORY . self::SRC_APP_ETC_DOTENV_FILE;
+        $dotenvDest = ROOT_DIRECTORY . self::DEST_APP_ETC_DOTENV_FILE;
         if (!file_exists($dotenvDest)) {
             copy($dotenvSrc, $dotenvDest);
         }
+
+        $envFile = ROOT_DIRECTORY . self::ENV_FILE;
         if (!file_exists($envFile)) {
             touch($envFile);
         }
@@ -39,8 +52,11 @@ class MagentoDotenv
      * @param string $debugKey
      * @return SymfonyDotenv
      */
-    public static function get(bool $usePutEnv = false, $envKey = 'APP_ENV', string $debugKey = 'APP_DEBUG')
-    {
+    public static function get(
+        bool $usePutEnv = false,
+        string $envKey = self::ENV_KEY,
+        string $debugKey = self::DEBUG_KEY
+    ) {
         $dotenv = new SymfonyDotenv($envKey, $debugKey);
         $dotenv->usePutEnv($usePutEnv);
         $dotenv->loadEnv(ROOT_DIRECTORY . self::ENV_FILE);
--- autoload.php
+++ autoload.php
@@ -5,6 +5,6 @@ use Zepgram\Component\MagentoDotenv\MagentoDotenv;
 
 $reflection = new \ReflectionClass(ClassLoader::class);
 $rootDirectory = dirname(dirname(dirname($reflection->getFileName())));
-define('ROOT_DIRECTORY', $rootDirectory);
+define('ROOT_DIRECTORY', $rootDirectory . DIRECTORY_SEPARATOR);
 
 MagentoDotenv::init();
